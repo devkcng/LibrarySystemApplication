ab688713a897550f0ca7616d6514431f57f8d148
devkcng <kimmanhcuong230304@gmail.com>
2023-12-04 21:36:24 +0700

    fix: lose attributes of Book class

--- BookClassLibrary/Book.cs
+++ BookClassLibrary/Book.cs
@@ -7,5 +7,25 @@ using System.Threading.Tasks;
 
 namespace BookClassLibrary
 {
+    public class Book
+    {
+        private string id;
+        private string title;
+        private string author;
+        private string status;
+        private string category;
+        public Book(string id, string title, string author, string status)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.status = status;
+        }
+
+        public string ID { get { return id; } set { id = value; } }
+        public string Title { get { return title; } set { title = value; } }
+        public string Author { get { return author; } set { author = value; } }
+        public string Status { get { return status; } set { status = value; } }
+        public string Category { get { return category; } set { category = value; } }
     }
 }
