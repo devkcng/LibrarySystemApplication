875ef31b3d534abd7525cb2e2b343753ddb78fcf
kiyotaka1102 <129610847+kiyotaka1102@users.noreply.github.com>
2023-12-04 17:48:13 +0700

    feat: add variable ISBN, Title, Author, Category.

---
+++ BookClassLibrary/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClassLibrary
+{
+    public class Book
+    {
+        string id;
+        string title;
+        string author;
+        string category;
+        public string ID {  get;}
+        public string Title { get;}
+        public string Author { get;}
+        public string Category { get;}
+        public Book() 
+        {
+        }
+        public Book(string id, string title, string author, string category)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.category = category;
+        }
+    }
+}
