93a1749f501c0a5bef7541dee344630fec00265b
devkcng <kimmanhcuong230304@gmail.com>
2023-12-14 15:57:54 +0700

    feat: add Key class for saving data in key file

---
+++ Dataloader/Key.cs
@@ -0,0 +1,16 @@
+﻿namespace Dataloader
+{
+    public class Key
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        
+        public string UserID { get; set; }
+        public Key(string username, string password, string userID)
+        {
+            Username = username;
+            Password = password;
+            UserID = userID;
+        }
+    }
+}
